86f446fef9c94626d8d114d14213587604e6dda0
zhangp <zhangp@linkcld.com>
2018-05-23 15:16:59 +0800

    初始化项目目录结构，包含maven、web、mybatis

---
+++ src/main/java/com/zpstudio/hw/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.zpstudio.hw;
+
+/**
+ * @Author zhangpeng
+ * @Date 2018/5/23 13:49
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ src/main/java/com/zpstudio/hw/domain/DBConfigMapper.java
@@ -0,0 +1,12 @@
+package com.zpstudio.hw.domain;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Description 使用数据源配置获取某个数据库用户下的mybatis的mapper
+ * @Author zhangpeng
+ * @Date 2018/5/23 13:59
+ **/
+@Mapper
+public class DBConfigMapper {
+}
